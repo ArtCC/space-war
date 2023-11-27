04dacc2f4f6d8b5c0fe4260355e26239f217efc0
Arturo Carretero Calvo <10163049+ArtCC@users.noreply.github.com>
2023-11-27 10:29:05 +0100

    Fix joystick touch position.

--- space-war/Scenes/GameScene+UITouch.swift
+++ space-war/Scenes/GameScene+UITouch.swift
@@ -12,70 +12,69 @@ import SpriteKit
 
 extension GameScene {
 
-  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    for touch in touches {
-      let touchLocation = touch.location(in: self)
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let touchLocation = touch.location(in: self)
 
-      if let node = atPoint(touchLocation) as? SKSpriteNode {
-        if node.name == Nodes.joystick.rawValue {
-          if (CGRectContainsPoint(joystick.frame, touchLocation)) {
-            joystickIsActive = true
-          } else {
-            joystickIsActive = false
-          }
-          selectedNodes[touch] = node
-        } else if node.name == Nodes.firePad.rawValue {
-          playerShot()
+            if let node = atPoint(touchLocation) as? SKSpriteNode {
+                if node.name == Nodes.joystick.rawValue {
+                    if (CGRectContainsPoint(joystick.frame, touchLocation)) {
+                        joystickIsActive = true
+                    } else {
+                        joystickIsActive = false
+                    }
+                    selectedNodes[touch] = node
+                } else if node.name == Nodes.firePad.rawValue {
+                    playerShot()
+                }
+            }
         }
-      }
     }
-  }
 
-  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-    for touch in touches {
-      let touchLocation = touch.location(in: self)
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let touchLocation = touch.location(in: self)
 
-      if let node = selectedNodes[touch] {
-        if joystickIsActive == true {
-          let vector = CGVector(dx: touchLocation.x - joystickBase.position.x,
-                                dy: touchLocation.y - joystickBase.position.y)
-          let angle = atan2(vector.dy, vector.dx)
-          let radio: CGFloat = joystickBase.frame.size.height / 2
-          let xDist: CGFloat = sin(angle - 1.57079633) * radio
-          let yDist: CGFloat = cos(angle - 1.57079633) * radio
+            if let node = selectedNodes[touch], node.name == Nodes.joystick.rawValue {
+                if joystickIsActive {
+                    let vector = CGVector(dx: touchLocation.x - joystickBase.position.x,
+                                          dy: touchLocation.y - joystickBase.position.y)
+                    let angle = atan2(vector.dy, vector.dx)
+                    let radio: CGFloat = joystickBase.frame.size.height / 2
 
-          if (CGRectContainsPoint(joystickBase.frame, touchLocation)) {
-            joystick.position = touchLocation
-          } else {
-            joystick.position = CGPointMake(joystickBase.position.x - xDist, joystickBase.position.y + yDist)
-          }
+                    let distance = min(sqrt(vector.dx * vector.dx + vector.dy * vector.dy), radio)
+                    let xDist: CGFloat = distance * cos(angle)
+                    let yDist: CGFloat = distance * sin(angle)
 
-          playerVelocityX = xDist / 49.0
-          playerVelocityY = yDist / 49.0
+                    joystick.position = CGPoint(x: joystickBase.position.x + xDist, y: joystickBase.position.y + yDist)
+                    
+                    let xDistPlayer: CGFloat = sin(angle - 1.57079633) * radio
+                    let yDistPlayer: CGFloat = cos(angle - 1.57079633) * radio
+
+                    playerVelocityX = xDistPlayer / radio
+                    playerVelocityY = yDistPlayer / radio
+                }
+            }
         }
-
-        node.position = touchLocation
-      }
     }
-  }
 
-  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-    for touch in touches {
-      if selectedNodes[touch] != nil {
-        if joystickIsActive == true {
-          let defaultPosition: SKAction = SKAction.move(to: joystickBase.position, duration: 0.05)
-          defaultPosition.timingMode = SKActionTimingMode.easeOut
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            if selectedNodes[touch] != nil {
+                if joystickIsActive == true {
+                    let defaultPosition: SKAction = SKAction.move(to: joystickBase.position, duration: 0.05)
+                    defaultPosition.timingMode = SKActionTimingMode.easeOut
 
-          joystick.run(defaultPosition)
+                    joystick.run(defaultPosition)
 
-          joystickIsActive = false
+                    joystickIsActive = false
 
-          playerVelocityX = 0
-          playerVelocityY = 0
+                    playerVelocityX = 0
+                    playerVelocityY = 0
+                }
+
+                selectedNodes[touch] = nil
+            }
         }
-
-        selectedNodes[touch] = nil
-      }
     }
-  }
 }
